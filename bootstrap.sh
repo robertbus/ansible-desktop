b43ed236c458403c234be7d26171f05c9024e1f9
Robert Bus <busrobert@gmail.com>
2018-09-13 18:25:31 +0200

    Update task

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,6 @@ apt-get update
 apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
-apt-get install -y ansible
+apt-get -y install ansible git
+
+ansible-pull -U https://github.com/robertbus/ansible-desktop.git -i localhost, setup.yml
